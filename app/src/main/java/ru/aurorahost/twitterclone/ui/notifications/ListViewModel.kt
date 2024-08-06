25ac889ff68998b3e858ee3abe020a245afc2255
IrinaB <balkarovairina@gmail.com>
2024-08-06 11:19:22 +0200

    Fix Some Styles

--- app/src/main/java/ru/aurorahost/twitterclone/ui/notifications/ListViewModel.kt
+++ app/src/main/java/ru/aurorahost/twitterclone/ui/notifications/ListViewModel.kt
@@ -1,13 +1,13 @@
-package ru.aurorahost.twitterclone.ui.dashboard
+package ru.aurorahost.twitterclone.ui.notifications
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ListViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
